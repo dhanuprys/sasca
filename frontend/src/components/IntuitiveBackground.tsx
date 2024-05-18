e8ccbec31f127bc96cbed2b56fb5c1a8c4286672
dhanu <dhanuprys@gmail.com>
2024-05-18 13:35:19 +0000

    Make className as optional

--- frontend/src/components/IntuitiveBackground.tsx
+++ frontend/src/components/IntuitiveBackground.tsx
@@ -1,10 +1,10 @@
 interface IntuitiveBackgroundProps {
-    className: string;
+    className?: string;
 }
 
 function IntuitiveBackground({ className }: IntuitiveBackgroundProps) {
     return (
-        <div className={`${className} absolute w-full top-0 left-0 bg-sky-800 h-[150px] -z-10 rounded-b-lg`}></div>
+        <div className={`${className || ''} absolute w-full top-0 left-0 bg-sky-800 h-[150px] -z-10 rounded-b-lg`}></div>
     );
 }
 
