cf95ad7994da21b13946d8218fc0c94b5b678289
Gede Dhanu Purnayasa <dhanuprys@gmail.com>
2024-05-20 19:09:52 +0800

    Header fix

--- frontend/src/app/counselor/schedule/page.tsx
+++ frontend/src/app/counselor/schedule/page.tsx
@@ -2,15 +2,15 @@ import IntuitiveBackground from "@/components/IntuitiveBackground";
 import BottomSpacer from "@/components/miscellaneous/BottomSpacer";
 import FlexColumn from "@/components/miscellaneous/FlexColumn";
 import BottomNavButton from "@/layouts/counselor/BottomNavButton";
+import CounselorHeader from "@/layouts/counselor/CounselorHeader";
 import SchoolSchedule from "@/layouts/counselor/SchoolSchedule";
-import StudentHeader from "@/layouts/student/StudentHeader";
 import CommonWrapper from "@/wrappers/CommonWrapper";
 
 export default function Home() {
   return (
     <div>
       <IntuitiveBackground />
-      <StudentHeader />
+      <CounselorHeader />
       <CommonWrapper className="mt-5">
         <FlexColumn>
             <SchoolSchedule />
